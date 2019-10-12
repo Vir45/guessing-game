6558df17aa5f6d5f62ab3792d047e724ae9aad97
Vova <vir42@mail.ru>
2019-10-12 15:35:07 +0300

    fix: some problem

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,67 +1,28 @@
 class GuessingGame {
     constructor() {
-        this.max;
-        this.arrOfAssumptions = [];
-        this.min = 1;
-        this.rand = null;
-    }
-  
-    setRange(min, max) {
-        this.min = 1;
-        this.max = max;
-    }
-  
-    guess() {
-  
-  
-        this.rand = Math.floor(this.min + Math.random() * (this.max + 1 - this.min));
-  
-        this.arrOfAssumptions.push(this.rand);
-  
-  
-        if (this.arrOfAssumptions.includes(this.rand)) {
-            this.rand = Math.floor(this.min + Math.random() * (this.max + 1 - this.min));
-        }
-  
-        return this.rand;
-  
-        
-    }
-  
-  
-    lower() {
-  
-        this.max = Math.round((this.max + this.min) / 2 - 1);
-  
-        this.rand = Math.floor(this.min + Math.random() * (this.max + 1 - this.min));
-  
-        this.arrOfAssumptions.push(this.rand);
-  
-  
-        if (this.arrOfAssumptions.includes(this.rand)) {
-            this.rand = Math.floor(this.min + Math.random() * (this.max + 1 - this.min));
-        }
-  
-  
-    }
-  
-    greater() {
-  
-        this.min = Math.round((this.max + this.min) / 2 + 1);
-  
-        this.rand = Math.floor(this.min + Math.random() * (this.max + 1 - this.min));
-  
-        this.arrOfAssumptions.push(this.rand);
-  
-  
-        if (this.arrOfAssumptions.includes(this.rand)) {
-            this.rand = Math.floor(this.min + Math.random() * (this.max + 1 - this.min));
-        }
-  
-      
-  
-    }
-}
+        this.min = null;
+        this.max = null;
+        this.center  = null;
+     }
+   
+     setRange(min, max) {
+       this.min = min;
+       this.max = max;
+     }
+   
+     guess() {
+       this.center = Math.ceil((this.max + this.min) / 2);
+       return this.center;
+     }
+   
+     lower() {
+       this.max = this.center;
+     }
+   
+     greater() {
+       this.min = this.center;
+     }
+  }
 
 
 module.exports = GuessingGame;
